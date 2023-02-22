37b489b658dc1694d25141297d23692c449619a0
Ashley Sun <janniesun00@gmail.com>
2023-02-21 23:30:05 -0500

    let's see if this works

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,9 @@ export default function Document() {
     return (
         <Html>
             <Head>
+            <link rel="manifest" href="/manifest.json" />
+            <link rel="apple-touch-icon" href="images/icons/cpday.png" />
+
                 <link
                     href="https://fonts.googleapis.com/css2?family=Eczar:wght@400;500;600;700;800&family=Inter:wght@300;400;500;600;700&display=swap"
                     rel="stylesheet"
